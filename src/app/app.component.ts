e045d04a5d30d6500870519c8d01e95db7b60115
akhilavipin <akhi.prabha@gmail.com>
2019-01-01 07:47:23 +0530

    Flipping Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'example-input';
   initialDataToChild = 'initalization';
+  parentData: { "name": string; };
   constructor() {
     this.getData();
   }
   getData() {
-    this.initialDataToChild = 'First Message';
+    this.parentData = {"name":"jghjg"}
+    // this.initialDataToChild = 'First Message';
   }
   sendMessage() {
     this.initialDataToChild = null;
@@ -21,4 +23,19 @@ export class AppComponent {
   sendMessageToChild() {
     this.initialDataToChild = 'First Message through sendMessageToChild()';
   }
+  add(){
+    
+    let y = document.getElementById('foo').classList;
+    y.toggle('card1');
+let a  = true;
+// if(a === true) {
+//   y.remove('card1')
+// }
+    // console.log('here')
+    // document.getElementById('bar').setAttribute("class", "front");
+  }
+  // add(){
+  //   document.getElementById('foo').setAttribute("class", "back");
+  //   document.getElementById('bar').setAttribute("class", "front");
+  // }
 }
